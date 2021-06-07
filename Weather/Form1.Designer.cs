2cb224bd9c1794f6a56beb3200974a907a461b90
xidios <47945350+xidios@users.noreply.github.com>
2021-06-08 01:50:59 +0700

    Добавьте файлы проекта.

---
+++ Weather/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.weatherState = new System.Windows.Forms.Label();
+            this.randomNumber = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // weatherState
+            // 
+            this.weatherState.AutoSize = true;
+            this.weatherState.Location = new System.Drawing.Point(41, 37);
+            this.weatherState.Name = "weatherState";
+            this.weatherState.Size = new System.Drawing.Size(47, 15);
+            this.weatherState.TabIndex = 0;
+            this.weatherState.Text = "Погода";
+            // 
+            // randomNumber
+            // 
+            this.randomNumber.AutoSize = true;
+            this.randomNumber.Location = new System.Drawing.Point(41, 73);
+            this.randomNumber.Name = "randomNumber";
+            this.randomNumber.Size = new System.Drawing.Size(52, 15);
+            this.randomNumber.TabIndex = 1;
+            this.randomNumber.Text = "Random";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(41, 117);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(219, 59);
+            this.start.TabIndex = 2;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ясно";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Облачно";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(196, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Пасмурно";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 192);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.randomNumber);
+            this.Controls.Add(this.weatherState);
+            this.Name = "Form1";
+            this.Text = "Random";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label weatherState;
+        private System.Windows.Forms.Label randomNumber;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Weather/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weather
+{
+    public enum Weather
+    {
+        Clear,
+        Cloudy,
+        Overcast
+    }
+    public partial class Form1 : Form
+    {
+        private Weather State = Weather.Clear;
+        private int HourCount = 0;
+        private int t = 0;
+        private int countClear = 0;
+        private int countCloudy = 0;
+        private int countOvercast = 0;
+
+        private Random rand = new Random();
+        private double[,] qMatrix = new double[3, 3] { { -0.4, 0.3, 0.1 }, { 0.4, -0.8, 0.4 }, { 0.1, 0.4, -0.5 } };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var q = rand.NextDouble() * (1 - (-1)) - 1;
+            randomNumber.Text = q.ToString();
+            Text = (HourCount / 24).ToString();
+            var currentDay = (int)State;
+            for(int i = 0; i < 3; i++)
+            {
+                if (i == currentDay)
+                    continue;
+
+                if (q > qMatrix[currentDay, i])
+                {
+                    State = (Weather)i;
+                    break;
+                }
+                else
+                {
+                    State = (Weather)currentDay;
+                }
+
+            }
+            switch (State)
+            {
+                case Weather.Clear:
+                    countClear++;
+                    label1.Text = $"Ясно {countClear}";
+                    break;
+                case Weather.Cloudy:
+                    countCloudy++;
+                    label2.Text = $"Облачно {countCloudy}";
+                    break;
+                case Weather.Overcast:
+                    countOvercast++;
+                    label3.Text = $"Пасмурно {countOvercast}";
+                    break;
+            }
+            HourCount++;
+            weatherState.Text = State.ToString();
+        }
+    }
+}
